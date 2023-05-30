b14a66203e6cb960fbb056e404cd4dba350a5622
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-05-30 13:42:40 -0700

    Modify About Page

--- frontend/react/src/pages/about.jsx
+++ frontend/react/src/pages/about.jsx
@@ -1,10 +1,39 @@
 import * as React from "react"
 import SinglePage from "../components/singlePage"
 import Seo from "../components/seo"
+import { GatsbyImage } from "gatsby-plugin-image";
+import { graphql } from "gatsby";
+
+const description = `
+
+`;
+
+export const query = graphql`
+query {
+    file(filter: {relativePath: {regex: "/portfolio/screencaps/"}}) {
+      nodes { 
+        name
+        childImageSharp {
+          gatsbyImageData(
+            width: 350
+            placeholder: BLURRED
+          )
+        }
+      }
+    }
+}
+`;
 
 export default function AboutPage(props) {
+    const { data } = props;
+
     return (
-        <SinglePage {...props} />
+        <SinglePage 
+            headline="About Us" 
+            description={description} 
+            imageRender={<GatsbyImage />}
+            {...props} 
+            />
     );
 }
 

c4d71efde05c47a22c3c70d15ed83a0ebec46aed
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-06-13 21:31:18 -0700

    Start Working on Shell

--- frontend/react/src/components/shell.jsx
+++ frontend/react/src/components/shell.jsx
@@ -1,14 +1,38 @@
 import * as React from "react";
 
+function submitCommandImpl({
+  event,
+  commands,
+  onSubmitCommand,
+  setCommandBufferNav,
+  commandBuffer,
+  setCommandBuffer,
+  textBuffer,
+  setTextBuffer,
+  inputBuffer,
+  setInputBuffer,
+}) {
+  event.preventDefault();
+
+
+
+}
+
 export default function SinglePage(props) {
   const { commands, onSubmitCommand, prefix } = props;
 
-  const [bufferNav, setBufferNav] = React.useState(-1);
-  const [buffer, setBuffer] = React.useState(["ATTACK", "FLEE", "ATTACK"]);
+  const [commandBufferNav, setCommandBufferNav] = React.useState(-1);
+  const [commandBuffer, setCommandBuffer] = React.useState([]);
+  const [textBuffer, setTextBuffer] = React.useState([]);
+  const [inputBuffer, setInputBuffer] = React.useState(
+    commandBufferNav > -1 && commandBufferNav < commandBuffer.length 
+      ? commandBuffer[commandBuffer.length - commandBufferNav - 1] 
+      : ""
+  );
   
   // Grab last 20 lines
   // Join by newline
-  const bufferFormatted = buffer.slice(buffer.length - 20, buffer.length).join('\n');
+  const bufferFormatted = textBuffer.slice(textBuffer.length - 20, textBuffer.length).join('\n');
 
   return (
     <section className="flex flex-col items-center bg-black">
@@ -23,12 +47,25 @@ export default function SinglePage(props) {
         <form
           className=" flex flex-row items-center w-full "
           action="none"
+          onSubmit={(event) => submitCommandImpl({
+            event,
+            commands,
+            onSubmitCommand,
+            setCommandBufferNav,
+            commandBuffer,
+            setCommandBuffer,
+            textBuffer,
+            setTextBuffer,
+            inputBuffer,
+            setInputBuffer,
+          })}
         >
           <div>{prefix || `>>>`}</div>
           <input
             className=" bg-transparent border-none w-full font-mono focus:ring-0 focus:outline-none "
             type="text"
-            defaultValue={ bufferNav > -1 && bufferNav < buffer.length ? buffer[buffer.length - bufferNav - 1] : ""}
+            value={inputBuffer}
+            onChange={(e) => setInputBuffer(e.target.value)}
           />
           <button
             className=" rounded-md bg-[#80CC23] p-2 text-black"
